c9168b6a62175186ec689290a226f15e53697196
fgle <fgle.sky@gmail.com>
2018-10-08 22:56:16 +0800

    新文件：   CMakeLists.txt 	新文件：   cmake-build-debug/CMakeCache.txt 	新文件：   cmake-build-debug/CMakeFiles/3.12.2/CMakeCCompiler.cmake 	新文件：   cmake-build-debug/CMakeFiles/3.12.2/CMakeCXXCompiler.cmake 	新文件：   cmake-build-debug/CMakeFiles/3.12.2/CMakeDetermineCompilerABI_C.bin 	新文件：   cmake-build-debug/CMakeFiles/3.12.2/CMakeDetermineCompilerABI_CXX.bin 	新文件：   cmake-build-debug/CMakeFiles/3.12.2/CMakeSystem.cmake 	新文件：   cmake-build-debug/CMakeFiles/3.12.2/CompilerIdC/CMakeCCompilerId.c 	新文件：   cmake-build-debug/CMakeFiles/3.12.2/CompilerIdCXX/CMakeCXXCompilerId.cpp 	新文件：   cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake 	新文件：   cmake-build-debug/CMakeFiles/CMakeOutput.log 	新文件：   cmake-build-debug/CMakeFiles/IntrodutionToAlgorithms.dir/DependInfo.cmake 	新文件：   cmake-build-debug/CMakeFiles/IntrodutionToAlgorithms.dir/build.make 	新文件：   cmake-build-debug/CMakeFiles/IntrodutionToAlgorithms.dir/cmake_clean.cmake 	新文件：   cmake-build-debug/CMakeFiles/IntrodutionToAlgorithms.dir/depend.make 	新文件：   cmake-build-debug/CMakeFiles/IntrodutionToAlgorithms.dir/flags.make 	新文件：   cmake-build-debug/CMakeFiles/IntrodutionToAlgorithms.dir/link.txt 	新文件：   cmake-build-debug/CMakeFiles/IntrodutionToAlgorithms.dir/progress.make 	新文件：   cmake-build-debug/CMakeFiles/Makefile.cmake 	新文件：   cmake-build-debug/CMakeFiles/Makefile2 	新文件：   cmake-build-debug/CMakeFiles/TargetDirectories.txt 	新文件：   cmake-build-debug/CMakeFiles/clion-environment.txt 	新文件：   cmake-build-debug/CMakeFiles/clion-log.txt 	新文件：   cmake-build-debug/CMakeFiles/cmake.check_cache 	新文件：   cmake-build-debug/CMakeFiles/feature_tests.bin 	新文件：   cmake-build-debug/CMakeFiles/feature_tests.c 	新文件：   cmake-build-debug/CMakeFiles/feature_tests.cxx 	新文件：   cmake-build-debug/CMakeFiles/progress.marks 	新文件：   cmake-build-debug/IntrodutionToAlgorithms.cbp 	新文件：   cmake-build-debug/Makefile 	新文件：   cmake-build-debug/cmake_install.cmake 	新文件：   main.cpp 	修改：     .gitignore

---
+++ cmake-build-debug/CMakeFiles/IntrodutionToAlgorithms.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IntrodutionToAlgorithms.dir/main.cpp.o"
+  "IntrodutionToAlgorithms.pdb"
+  "IntrodutionToAlgorithms"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IntrodutionToAlgorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
